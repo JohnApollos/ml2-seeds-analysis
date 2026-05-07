0ded86bcd61d04ecb836e33320cfc84e102bf266
Lewis <lewiskinyuanjue.ke@gmail.com>
2026-05-07 17:40:29 +0300

    done doing training supervised algorithm and removed rendadant code

--- ml2-seeds-analysis-main/setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-# make sure to make this file executable with `chmod +x setup.sh` 
-python3 -m venv .venv;  # create a virutal enviroment
-./.venv/bin/pip install pip --upgrade # update pip 
-./.venv/bin/pip install -r requirements.txt # install requirements files 
-
--- setup.sh
+++ setup.sh
@@ -3,4 +3,3 @@
 python3 -m venv .venv;  # create a virutal enviroment
 ./.venv/bin/pip install pip --upgrade # update pip 
 ./.venv/bin/pip install -r requirements.txt # install requirements files 
-
